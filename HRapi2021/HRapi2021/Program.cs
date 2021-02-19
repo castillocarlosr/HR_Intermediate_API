ed9d47f7e01e51d41bcc641fbc516f5fd4dcf154
Carlos Roberto Castillo <39848540+castillocarlosr@users.noreply.github.com>
2021-02-19 14:28:09 -0800

    added sln

---
+++ HRapi2021/HRapi2021/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HRapi2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello CARLOS!");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Your input:  {input}");
+        }
+    }
+}
