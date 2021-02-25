3d6c3377dd8fe0813c3f708c5d25eb90933b0f02
Carlos Roberto Castillo <39848540+castillocarlosr@users.noreply.github.com>
2021-02-24 16:20:41 -0800

    works on some key/value json pairs.  Not all thought?

--- HRapi2021/HRapi2021/Constants/RequestConstants.cs
+++ HRapi2021/HRapi2021/Constants/RequestConstants.cs
@@ -8,6 +8,13 @@ namespace HRapi2021.Constants
     {
         public const string BaseUrl = "https://jsonmock.hackerrank.com";
         public const string Url = "https://jsonmock.hackerrank.com/api/football_matches?year=2011&team1=Barcelona&page=1";
+        public static string ReturnPages(string team, int year) 
+        {
+            string temp = $"https://jsonmock.hackerrank.com/api/football_matches?"+year+"=2011&team1="+team+"&page=1";
+            return temp;
+        }
+
+
         public const string UserAgent = "User-Agent";
         public const string UserAgentValue = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
     }
