e04eb4e00e91d29858300897b7fdd94947fa271f
Carlos Roberto Castillo <39848540+castillocarlosr@users.noreply.github.com>
2021-02-26 13:14:43 -0800

    inital git REST API GET call running

--- WebAPIClient/Program.cs
+++ WebAPIClient/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace WebAPIClient
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly HttpClient client = new HttpClient();
+        
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Like all modern .NET APIs, HttpClient supports only async methods for its long-running APIs.");
+            Console.WriteLine(" Start by making an async method. ");
+            
+            await ProcessRepositories();
+        }
+
+        private static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+
+            var msg = await stringTask;
+            Console.Write(msg);
         }
     }
 }
